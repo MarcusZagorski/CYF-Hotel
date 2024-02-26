235b8e1427704b43dd17f0c3a3d35fcdd955cecd
Marcus <marcus.ats94@gmail.com>
2024-02-26 22:54:16 +0000

    Made changes to Restaurant.jsx

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import RestaurantButton from "./RestaurantButton";
+import Order from "../Order/Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  const orderOne = () => {
-    setOrders(orders + 1);
-  };
   return (
     <section className="restaurant">
       <h3 className="restaurant__heading">Restaurant Orders</h3>
       <ul className="restaurant__list">
-        <li className="restaurant__item">
-          Pizzas: {orders} <RestaurantButton orderOne={orderOne} />
-        </li>
+        <Order />
       </ul>
     </section>
   );
