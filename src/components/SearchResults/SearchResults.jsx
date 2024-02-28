e68f50c7b8dbc6918bbff72db8cfc35707281ded
Marcus <marcus.ats94@gmail.com>
2024-02-28 13:27:16 +0000

    Refactored search results and completed activerow feature

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,34 +1,56 @@
+import { useState } from "react";
+import "./SearchResults.scss";
 import dayjs from "dayjs";
 
-const SearchResults = ({ results }) => {
-  const tableTitles = Object.keys(results[0]);
-
+function SearchResults({ results }) {
+  const rowTitles = Object.keys(results[0]);
+  const rowsData = results;
   return (
-    <table style={{ textAlign: "center" }}>
-      <tbody>
+    <table>
+      <thead>
         <tr>
-          {tableTitles.map((title, index) => {
-            return <td key={index}>{title}</td>;
+          {rowTitles.map((rowHeader, rowHeaderIndex) => {
+            return <th key={rowHeaderIndex}>{rowHeader}</th>;
           })}
-          <td>Number of Nights</td>
+          <th>Number of Nights</th>
         </tr>
+      </thead>
+      <tbody>
+        {rowsData.map((rowData, rowDataIndex) => {
+          const numberOfNights = dayjs(rowData.checkOutDate).diff(
+            dayjs(rowData.checkInDate),
+            "day"
+          );
 
-        {results.map((result, resultIndex) => (
-          <tr key={resultIndex}>
-            {Object.values(result).map((value, valueIndex) => {
-              return <td key={valueIndex}>{value}</td>;
-            })}
-            <td>
-              {dayjs(result.checkOutDate).diff(
-                dayjs(result.checkInDate),
-                "day"
-              )}
-            </td>
-          </tr>
-        ))}
+          const [rowColor, setRowColor] = useState(false);
+
+          const styleRowColor = () => {
+            setRowColor((rowColor) => !rowColor);
+          };
+
+          const changeColorState = rowColor ? "table__row" : "";
+          return (
+            <tr
+              key={rowDataIndex}
+              style={{ textAlign: "center" }}
+              onClick={styleRowColor}
+              className={changeColorState}
+            >
+              <td>{rowData.id}</td>
+              <td>{rowData.title}</td>
+              <td>{rowData.firstName}</td>
+              <td>{rowData.surname}</td>
+              <td>{rowData.email}</td>
+              <td>{rowData.roomId}</td>
+              <td>{rowData.checkInDate}</td>
+              <td>{rowData.checkOutDate}</td>
+              <td>{numberOfNights}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
-};
+}
 
 export default SearchResults;
