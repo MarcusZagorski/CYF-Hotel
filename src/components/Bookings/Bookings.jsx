8818b47ed41815853ea59e4f1d9c5defd865072e
Marcus <marcus.ats94@gmail.com>
2024-03-05 15:54:54 +0000

    Completed the searchState feature

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,14 +1,20 @@
 import Search from "@/components/Search/Search";
 import SearchResults from "@/components/SearchResults/SearchResults.jsx";
 import FakeBookings from "@/data/fakeBookings.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Bookings = () => {
+  useEffect(() => {});
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
 
   const [bookings, setBookings] = useState(FakeBookings);
+  // useEffect(() => {
+  //   fetch("https://api.tvmaze.com/shows/22/episodes")
+  //     .then((res) => res.json())
+  //     .then((data) => setBookings(data));
+  // });
 
   return (
     <main className="bookings">
--- src/components/Deck/Deck.test.jsx
+++ src/components/Deck/Deck.test.jsx
@@ -1,9 +1,18 @@
 import Deck from "./Deck.jsx";
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import Card from "../Card/Card.jsx";
+import cardsData from "@/data/fakeCards.json";
 
 describe("Deck", () => {
-  it("should have a test, please write one", () => {
+  it("Should render cards", () => {
+    // render(<Deck />);
+    // cardsData.forEach((card) => {
+    //   render(
+    //     <Card title={card.title} image={card.image} anchorTag={card.url} />
+    //   );
+    //   expect(Card).toBeInTheDocument(Deck);
+    // });
     return true;
   });
 });
