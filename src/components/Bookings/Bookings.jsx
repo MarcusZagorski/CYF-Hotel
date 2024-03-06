9ea57a3da81873b80beb0a957a32d660eeea2636
Marcus <marcus.ats94@gmail.com>
2024-03-06 00:26:43 +0000

    Completed the search functionality

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,25 +1,27 @@
 import Search from "@/components/Search/Search";
 import SearchResults from "@/components/SearchResults/SearchResults.jsx";
 import FakeBookings from "@/data/fakeBookings.json";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Bookings = () => {
-  useEffect(() => {});
-
   const [bookings, setBookings] = useState(FakeBookings);
-  // useEffect(() => {
-  //   fetch("https://api.tvmaze.com/shows/22/episodes")
-  //     .then((res) => res.json())
-  //     .then((data) => setBookings(data));
-  // });
+
   const handleSearch = (searchInput) => {
-    console.log(searchInput);
+    bookings.filter((booking) => {
+      if (
+        booking.firstName === searchInput ||
+        booking.surname === searchInput
+      ) {
+        setBookings([booking]);
+      }
+    });
   };
 
   return (
     <main className="bookings">
       <Search onSearchSubmit={handleSearch} />
       <SearchResults results={bookings} />
+      {console.log(bookings)}
     </main>
   );
 };
